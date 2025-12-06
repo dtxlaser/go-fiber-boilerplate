934cf14a69679a4f821190029463fb86a95940fa
dtxlaser <dtx.laser@gmail.com>
2025-12-06 15:11:17 +0600

    Update database.go

--- src/database/database.go
+++ src/database/database.go
@@ -1,40 +1,50 @@
 package database
 
 import (
-	"app/src/utils"
-	"fmt"
-	"time"
+    "app/src/utils"
+    "fmt"
+    "os"
+    "strconv"
+    "time"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
+    "gorm.io/gorm/logger"
 )
 
-func Connect(dbHost, dbName string) *gorm.DB {
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Shanghai",
-		dbHost, config.DBUser, config.DBPassword, dbName, config.DBPort,
-	)
+func Connect() *gorm.DB {
+    port, err := strconv.Atoi(os.Getenv("DB_PORT"))
+    if err != nil {
+        utils.Log.Fatalf("Invalid DB_PORT: %v", err)
+    }
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		Logger:                 logger.Default.LogMode(logger.Info),
-		SkipDefaultTransaction: true,
-		PrepareStmt:            true,
-		TranslateError:         true,
-	})
-	if err != nil {
-		utils.Log.Errorf("Failed to connect to database: %+v", err)
-	}
+    dsn := fmt.Sprintf(
+        "host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Shanghai",
+        os.Getenv("DB_HOST"),
+        os.Getenv("DB_USER"),
+        os.Getenv("DB_PASSWORD"),
+        os.Getenv("DB_NAME"),
+        port,
+    )
 
-	sqlDB, errDB := db.DB()
-	if errDB != nil {
-		utils.Log.Errorf("Failed to connect to database: %+v", errDB)
-	}
+    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+        Logger:                 logger.Default.LogMode(logger.Info),
+        SkipDefaultTransaction: true,
+        PrepareStmt:            true,
+        TranslateError:         true,
+    })
+    if err != nil {
+        utils.Log.Fatalf("Failed to connect to database: %+v", err)
+    }
 
-	// Config connection pooling
-	sqlDB.SetMaxIdleConns(10)
-	sqlDB.SetMaxOpenConns(100)
-	sqlDB.SetConnMaxLifetime(60 * time.Minute)
+    sqlDB, errDB := db.DB()
+    if errDB != nil {
+        utils.Log.Fatalf("Failed to get DB instance: %+v", errDB)
+    }
 
-	return db
+    sqlDB.SetMaxIdleConns(10)
+    sqlDB.SetMaxOpenConns(100)
+    sqlDB.SetConnMaxLifetime(60 * time.Minute)
+
+    return db
 }
